7000f7bb4694341fa5548ca599482f4c777e801a
Zavizhenetc <zavizhenetc@gmail.com>
2020-09-26 22:02:28 +0300

    добавлен слушатель , выбор карточек

--- src/index.js
+++ src/index.js
@@ -1,23 +1,40 @@
-function csl(elem){
-  console.log(elem)
+function csl(elem) {
+  console.log(elem);
 }
 
-import "./index.css"; 
-import CardRender from '../src/js/components/CardRender';
-import Card from '../src/js/components/Card.js';
-import {CARDS_ITEMS} from '../src/js/constants/constants.js';
-const template = document.querySelector('#items-container').content;
-  // const template = document.querySelector('#items-container').content.querySelector('.items-container__item');
-  // csl(template);
-  const itemsList = document.querySelector('.items-container');
-  // const cardRender = new CardRender(itemsList);
+import "./index.css";
+import CardRender from "../src/js/components/CardRender";
+import Card from "../src/js/components/Card.js";
+import { CARDS_ITEMS } from "../src/js/constants/constants.js";
+const template = document
+  .querySelector("#items-container")
+  .content.querySelector(".items-container__item");
+const itemsList = document.querySelector(".items-container");
 
-  const initialCards = CARDS_ITEMS.map(({ name, stock, weight }) => {
-    return new Card(name, stock, template, weight ).createCard();
-  });
+const initialCards = CARDS_ITEMS.map(({ name, stock, weight }) => {
+  return new Card(name, stock, template, weight).createCard();
+});
 
-  csl(initialCards);
-  const listItems = new CardRender(itemsList).render(initialCards);
+const listItems = new CardRender(itemsList).render(initialCards);
 
+// =====================
+const items = document.querySelector(".items-container");
+const tmpl = document.querySelector(".item-container");
+const cardItem = document.querySelector(".items-container__item");
+const card = document.querySelector(".items-container__cover");
+
+
+
+
+
+function cardSelected(evt) {
+  const target = evt.target.closest(".items-container__cover");
+  const corner = target.closest(".items-container__item").querySelector('.items-container__corner-clip');
+  target.classList.toggle("selected");
+  corner.classList.toggle("selected");
+}
+
+
+items.addEventListener("click", cardSelected)
 
 
